fa18f827d7235d0935be2b1f7eb0a672de86e59f
Daniel Chan <iam@danielchan.me>
2017-01-30 16:25:35 -0500

    Documentation added for the functions. Some more code clean up.

--- java/src/Main/Proxy.java
+++ java/src/Main/Proxy.java
@@ -17,8 +17,11 @@ public class Proxy {
   private int localPortNumber, remotePortNumber;
   private String hostName, destinationUrl;
 
-  // Initialize the proxy server, by default webpages are on port 80, but the client can
-  // specify others if he wants to.
+  /**
+   * Default constructor for Proxy, with a parameter of local port, to listen
+   * for connections on.
+   * @param localPort - The port which we listen for incoming connections.
+   */
   public Proxy(int localPort) {
     this.localPortNumber = localPort;
     this.remotePortNumber = 80;
@@ -26,49 +29,75 @@ public class Proxy {
 
   }
 
-  private byte[] extractBytes(InputStream clientRequest, boolean replaceHost) {
+  /**
+   * This method extracts information from the InputStream either from the
+   * client or the server. We extract the destination address, and assign it
+   * to this loop iterations destination address.
+   *
+   * Here we also modify the requests coming from the client by passing it
+   * along to another method, removeHost, specifically,
+   * we modify the GET/POST parameters to retrieve and access certain
+   * documents on the domain.
+   *
+   * @param incomingStream - The InputStream that we are extracting
+   *                      information from.
+   * @param replaceHost - A true/false, to indicate whether we are to replace
+   *                    the host in the HTTP request/response.
+   * @return Returns the content in an array of bytes to return to the
+   * client, or for the proxy to pass to the destination.
+   */
+  private byte[] extractBytes(InputStream incomingStream, boolean replaceHost) {
 
     String requestContent;
+    // Abnormally high because we need to compensate for partial reads issue
+    // in initialization.
     byte[] buffer = new byte[100000];
 
     try {
       ByteArrayOutputStream baos = new ByteArrayOutputStream();
       int len = 0;
-      while ((len = clientRequest.read(buffer)) > 0) {
+      while ((len = incomingStream.read(buffer)) > 0) {
         baos.write(buffer, 0, len);
         baos.flush();
         break;
       }
       requestContent = new String(baos.toByteArray());
 
-      // Debugging purposes, seeing the POST and GET requests from client.
-//      System.out.println(requestContent);
       if (replaceHost) {
         this.hostName = extractHost(requestContent);
         return removeHost(baos);
       }
     } catch (IOException ioException) {
-      System.out.println("Failed to close stream" + ioException);
+      printToConsole("Failed to close stream" + ioException);
     }
     return buffer;
   }
 
 
+  /**
+   * This removes the hostname from the GET/POST request. For example, say we
+   * have a GET request with the following format.
+   * GET /proxy/http://httpbin.org/get
+   * It would reformat to GET /get.
+   *
+   * @param request The client's InputStream, in ByteArray form to manipulate
+   *                with.
+   * @return Returns the byte representation of the modified HTTP request.
+   */
   private byte[] removeHost(ByteArrayOutputStream request) {
 
     String clientReq = new String(request.toByteArray());
     int startingIndex = clientReq.indexOf(this.destinationUrl);
     int endingIndex = clientReq.indexOf(this.hostName) + this.hostName.length();
 
-    if (startingIndex <= 0) {
+    // It can never be 0. The index has to be greater than the starting index.
+    if (endingIndex == 0) {
+      endingIndex = clientReq.indexOf(this.destinationUrl) + this.destinationUrl.length();
+    } else if (startingIndex <= 0) {
       return clientReq.getBytes();
     }
-    // Debug message.
-//    System.out.println("REMOVING: " + clientReq.substring(startingIndex - 1,
-//        endingIndex));
 
     StringBuilder sb = new StringBuilder(clientReq);
-
     sb.delete(startingIndex - 1, endingIndex);
 
     // The case where the user inputs http://google.com with no / at the end
@@ -76,14 +105,7 @@ public class Proxy {
     if (clientReq.charAt(endingIndex) == ' ') {
       sb.insert(4, '/');
     }
-
-    // Debug message.
-//    System.out.println("----- ADJUSTED HTTP REQ ------");
-//    System.out.println(sb.toString());
-//    System.out.print("----- END OF ADJUSTED   ------\n");
-
     return sb.toString().getBytes();
-
   }
 
   /*
@@ -95,22 +117,38 @@ public class Proxy {
   private String extractHost(String requestContent) {
 
     String[] requestSplit = requestContent.split(" ");
-
     this.destinationUrl = requestSplit[1].substring(1, requestSplit[1].length());
-
     String[] urlSplit = this.destinationUrl.split("/");
 
-    if (urlSplit[0].equalsIgnoreCase("http:")) {
-      return urlSplit[2];
+    if(urlSplit[0].equals("proxy") && urlSplit.length >= 2) {
+      if (urlSplit[1].equalsIgnoreCase("http:")) {
+        return urlSplit[3];
+      } else {
+        return urlSplit[1];
+      }
     } else {
-      return urlSplit[0];
+      return "";
     }
   }
 
+  /**
+   * Generic method to print to console with the timestamp and the message.
+   * @param text - message to be printed to console.
+   */
   public static void printToConsole(String text) {
     System.out.println("[" + LocalDateTime.now() + "] " + text);
   }
 
+  /**
+   * Initialization method, for initiating the server socket, to whatever was
+   * defined in the main method in Driver, otherwise it defaults to the value
+   * of 8000.
+   *
+   * We also listen for any incoming connections, and when we get a incoming
+   * connection we process the request content and extract the destination
+   * URL and modify the request content, then pass it through to the
+   * destination.
+   */
   private void initialize() {
     try {
       ServerSocket serverListen = new ServerSocket(this.localPortNumber);
@@ -164,10 +202,9 @@ public class Proxy {
           */
           Thread tunnelClientInfo = new Thread() {
             public void run() {
-
               try {
-                  toServer.write(req, 0, req.length);
-                  toServer.flush();
+                toServer.write(req, 0, req.length);
+                toServer.flush();
 
               } catch (IOException ioError) {
                 printToConsole("ERROR: Something happened to the client's "
@@ -185,34 +222,28 @@ public class Proxy {
 
           try {
 
-              toClient.write(response, 0, response.length);
-              toClient.flush();
+            toClient.write(response, 0, response.length);
+            toClient.flush();
 
           } catch (IOException ioError) {
             printToConsole("ERROR: Something happened to the server's socket" +
                 ". Unable to read from it." + ioError);
           }
 
-        } finally {
-          try {
-            if (server != null) {
-              server.close();
-            }
-            if (client != null) {
-              client.close();
-            }
-          } catch (IOException e) {
-            printToConsole("ERROR: Failed to close server/client sockets."
-                + e);
+          if (server != null) {
+            server.close();
           }
+          if (client != null) {
+            client.close();
+          }
+
+        } catch (IOException ioE) {
+
         }
       }
-
     } catch (IOException ioError) {
       printToConsole("ERROR: Port in use, failed to initialize." + ioError);
       System.exit(1);
     }
   }
-
-
 }
