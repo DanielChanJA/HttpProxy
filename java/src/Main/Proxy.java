2b8dc5f498e017909629bc4980c26d5868b76168
Daniel Chan <iam@danielchan.me>
2017-01-30 10:26:38 -0500

    Added logging to console. Appropriate timestamps and debug messages.

--- java/src/Main/Proxy.java
+++ java/src/Main/Proxy.java
@@ -4,13 +4,13 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 /**
  * Created by Daniel Chan on 28/1/2017.
- *
+ * <p>
  * Supports HTTP 1.1 GET/POST requests, DOES NOT SUPPORT HTTPS. WILL BREAK!
  */
 public class Proxy {
@@ -35,7 +35,7 @@ public class Proxy {
     try {
       ByteArrayOutputStream baos = new ByteArrayOutputStream();
       int len = 0;
-      while((len = clientRequest.read(buffer)) > 0) {
+      while ((len = clientRequest.read(buffer)) > 0) {
         baos.write(buffer, 0, len);
         baos.flush();
         break;
@@ -44,10 +44,8 @@ public class Proxy {
 
       // Debugging purposes, seeing the POST and GET requests from client.
 //      System.out.println(requestContent);
-
-      if(replaceHost) {
+      if (replaceHost) {
         this.hostName = extractHost(requestContent);
-
         return removeHost(baos);
       }
     } catch (IOException ioException) {
@@ -63,10 +61,9 @@ public class Proxy {
     int startingIndex = clientReq.indexOf(this.destinationUrl);
     int endingIndex = clientReq.indexOf(this.hostName) + this.hostName.length();
 
-    if(startingIndex <= 0) {
+    if (startingIndex <= 0) {
       return clientReq.getBytes();
     }
-
     // Debug message.
 //    System.out.println("REMOVING: " + clientReq.substring(startingIndex - 1,
 //        endingIndex));
@@ -77,7 +74,7 @@ public class Proxy {
 
     // The case where the user inputs http://google.com with no / at the end
     // of the url.
-    if(clientReq.charAt(endingIndex) == ' ') {
+    if (clientReq.charAt(endingIndex) == ' ') {
       sb.insert(4, '/');
     }
 
@@ -104,20 +101,21 @@ public class Proxy {
 
     String[] urlSplit = this.destinationUrl.split("/");
 
-    if(urlSplit[0].equalsIgnoreCase("http:")) {
+    if (urlSplit[0].equalsIgnoreCase("http:")) {
       return urlSplit[2];
     } else {
       return urlSplit[0];
     }
   }
 
+  private static void printToConsole(String text) {
+    System.out.println("[" + LocalDateTime.now() + "] " + text);
+  }
+
   private void initialize() {
-
-    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-
     try {
       ServerSocket serverListen = new ServerSocket(this.localPortNumber);
-      System.out.println("["+ timestamp.toString() + "]" +" Listening on " +
+      System.out.println("[" + LocalDateTime.now() + "]" + " Listening on " +
           "port: " + this.localPortNumber);
 
 
@@ -129,34 +127,30 @@ public class Proxy {
 
           client = serverListen.accept();
 
-          System.out.println("["+ timestamp.toString() + "]" + " Incoming " +
-              "request" +
-              " " +
-              "from " + client
-              .getRemoteSocketAddress().toString());
+          String clientIp = client.getRemoteSocketAddress().toString();
+
+
+          printToConsole("Incoming request from " + clientIp);
 
           final InputStream fromClient = client.getInputStream();
           final OutputStream toClient = client.getOutputStream();
 
           final byte[] req = extractBytes(fromClient, true);
 
-//          System.out.println(requestContent);
-
-          // Debugging purpose, to see what the host is being set to.
-          System.out.println("["+ timestamp.toString() + "] " +client
-              .getRemoteSocketAddress().toString() + " " +
-              "attempting to connect to " + this.destinationUrl);
+          printToConsole(clientIp +
+              " attempting to connect to " + this.destinationUrl);
 
           try {
             server = new Socket(this.hostName, this.remotePortNumber);
           } catch (IOException ioError) {
-            System.out.println("Unable to connect to " + this.hostName + " on port " +
-                this.remotePortNumber);
+            printToConsole("Unable to connect to " + this.hostName +
+                " on port " + this.remotePortNumber);
             client.close();
             continue;
           }
 
-          System.out.println("["+ timestamp.toString() + "] " +"Successfully connected to " + this.destinationUrl);
+          printToConsole("Successfully connected to " +
+              this.destinationUrl);
 
           final InputStream fromServer = server.getInputStream();
           final OutputStream toServer = server.getOutputStream();
@@ -168,8 +162,8 @@ public class Proxy {
                 toServer.flush();
 
               } catch (IOException ioError) {
-                System.out.println("Something happened to the client's socket. Unable" +
-                    " to read info." + ioError);
+                printToConsole("ERROR: Something happened to the client's "
+                    + "socket. Unable to read info." + ioError);
               }
             }
           };
@@ -178,43 +172,37 @@ public class Proxy {
           // proxy streams start at the same time.
           tunnelClientInfo.start();
 
-
           byte[] response = extractBytes(fromServer, false);
 
-          // Hangs here for some weird reason, unable to debug why it hangs
-          // here. FIXED.
           try {
-              toClient.write(response, 0, response.length);
-              toClient.flush();
+            toClient.write(response, 0, response.length);
+            toClient.flush();
 
           } catch (IOException ioError) {
-            System.out.println("Something happened to the server's socket. Unable to read" +
-                " from it." + ioError);
+            printToConsole("ERROR: Something happened to the server's socket" +
+                ". Unable to read from it." + ioError);
           }
 
-        } finally{
-            try {
-              if (server != null) {
-                server.close();
-              }
-              if (client != null) {
-                client.close();
-              }
-            } catch (IOException e) {
-              System.out.println("Failed to close server/client sockets." + e);
+        } finally {
+          try {
+            if (server != null) {
+              server.close();
             }
+            if (client != null) {
+              client.close();
+            }
+          } catch (IOException e) {
+            printToConsole("ERROR: Failed to close server/client sockets."
+                + e);
           }
-
         }
-
-      } catch(IOException ioError){
-        System.out.println(ioError);
-        System.out.println("ERROR: Port in use, failed to initialize.");
-        System.exit(1);
       }
 
-
+    } catch (IOException ioError) {
+      printToConsole("ERROR: Port in use, failed to initialize." + ioError);
+      System.exit(1);
     }
-
-
   }
+
+
+}
